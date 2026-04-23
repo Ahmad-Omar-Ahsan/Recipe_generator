eec6d6db4428ef81da1b57f175babaeeec6cf2ba
Ahmad Omar Ahsan <ahmadomarahsan@gmail.com>
2026-04-23 16:09:39 -0600

    still working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def main():
     print("Hello from recipe-generator!")
-
+    df = pd.read_csv("dataset/Multi_Cuisine_Recipe_Dataset.csv")
 
 if __name__ == "__main__":
     main()
