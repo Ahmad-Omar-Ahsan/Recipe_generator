6c3a76c10335c2b4afb2f97b5536a33d6bf10596
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2026-04-26 13:11:01 -0600

    Still working

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     print("Hello from recipe-generator!")
     df = pd.read_csv("dataset/Multi_Cuisine_Recipe_Dataset.csv")
     df.head()
+    print(df.columns)
 
 if __name__ == "__main__":
     main()
