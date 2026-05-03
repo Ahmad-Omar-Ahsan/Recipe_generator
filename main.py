6c3f263c202101cc4524e00ec9acd5d449675c9a
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2026-05-03 12:17:33 -0600

    Still working

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     print("Generated Recipe:")
     print(generated_text)
-    x = 1
+    x = df['recipe']
 
 if __name__ == "__main__":
     main()
